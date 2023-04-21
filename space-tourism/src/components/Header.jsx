9510ab770a399df194badd17824b668f074f2ee4
DavideCruciani <cruciani240203@gmail.com>
2023-04-21 21:39:31 +0200

    fixed some bugs

--- space-tourism/src/components/Header.jsx
+++ space-tourism/src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     <header className='fixed flex h-0 sm:h-auto justify-between lg:items-center w-full top-0 z-50 lg:p-12 pl-12'>
       <div className='py-8'>
 
-        <div className='bg-white rounded-full hidden sm:block'>
+        <div className='bg-white rounded-full'>
           <img src={Logo} alt="Logo" />
         </div>
         
@@ -26,8 +26,8 @@ const Header = () => {
       <button className={showNav ? 'block sm:hidden z-[9999] w-6 aspect-[1] bg-no-repeat bg-cover border-0 bg-[url(/src/assets/shared/icon-close.svg)] absolute right-8 top-8' : 'block sm:hidden z-[9999] w-6 aspect-[1] bg-no-repeat bg-cover border-0 bg-[url(/src/assets/shared/icon-hamburger.svg)] absolute right-8 top-8'} onClick={ShowNavbar}></button>
 
       <div className={showNav ? "bg-[hsla(0,0%,100%,.05)] w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-blur-[30px] translate-x-0 ease duration-500 sm:duration-0" : "bg-[hsla(0,0%,100%,.05)] w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-blur-[30px] translate-x-full sm:translate-x-0 ease-out sm:duration-0"}>
-        <nav className='flex flex-col py-40 sm:py-0 sm:pl-0 sm:flex-row w-full gap-8 lg:gap-14 h-full list-none text-md tracking-widest lg:text-lg text-white font-BarlowCD'>
-
+        <nav className='flex py-40 sm:py-0 sm:pl-0 sm:flex-row w-full lg:gap-14 h-full list-none text-md tracking-widest lg:text-lg text-white font-BarlowCD'>
+          <ul className='flex flex-col sm:flex-row w-full gap-8'>
           <li className='sm:h-full mx-auto w-4/6 flex items-center relative'><NavLink className={({ isActive }) => {
             return isActive ? "border-b-4 border-white h-full flex items-center" : "border-b-2 border-transparent full flex items-center"
           }}
@@ -47,6 +47,9 @@ const Header = () => {
           <li className='sm:h-full mx-auto w-4/6 flex items-center'><NavLink className={({ isActive }) => {
             return isActive ? "border-b-4 border-white h-full flex items-center" : "border-b-2 border-transparent full flex items-center"
           }} to="/technology"><span className='font-bold mr-2 block sm:hidden lg:block'>03</span>TECHNOLOGY</NavLink></li>
+
+          </ul>
+          
         </nav>
       </div>
     </header>
