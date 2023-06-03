3242d6f71676b905dd806d3c3267e02c3a9d0c59
DavideCruciani <cruciani240203@gmail.com>
2023-06-03 16:04:43 +0200

    navbar background
    navbar background fix for search engines that doesn't support backdrop-filter

--- space-tourism/src/components/Header.jsx
+++ space-tourism/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Logo from '../assets/shared/logo.svg'
 import { NavLink } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
 
@@ -24,7 +25,7 @@ const Header = () => {
 
       <button aria-label='menu' className={showNav ? 'block sm:hidden z-[9999] w-6 aspect-[1] bg-no-repeat bg-cover border-0 bg-[url(/src/assets/shared/icon-close.svg)] absolute right-8 top-8' : 'block sm:hidden z-[9999] w-6 aspect-[1] bg-no-repeat bg-cover border-0 bg-[url(/src/assets/shared/icon-hamburger.svg)] absolute right-8 top-8'} onClick={ShowNavbar}></button>
 
-      <div className={showNav ? "bg-[hsla(0,0%,100%,.05)] w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-blur-[30px] translate-x-0 ease duration-500 sm:duration-0" : "bg-[hsla(0,0%,100%,.05)] w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-blur-[30px] translate-x-full sm:translate-x-0 ease-out sm:duration-0"}>
+      <div className={showNav ? "w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-support translate-x-0 ease duration-500 sm:duration-0" : "w-3/5 sm:w-auto lg:pl-24 sm:px-10 h-[100vh] sm:h-24 lg:pr-44 flex justify-center shadow-[-4px_4px_16px_3px_#0b0d1766] backdrop-support translate-x-full sm:translate-x-0 ease-out sm:duration-0"}>
         <nav className='flex py-40 sm:py-0 sm:pl-0 sm:flex-row w-full lg:gap-14 h-full list-none text-md tracking-widest lg:text-lg text-white font-BarlowCD'>
           <ul className='flex flex-col sm:flex-row w-full gap-8'>
           <li className='sm:h-full mx-auto w-4/6 flex items-center relative'><NavLink className={({ isActive }) => {
